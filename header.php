ae12e3dccb93c693d0312c294a8c5f7474f92110
Oscar Mwananandimai <scarwizkid@gmail.com>
2014-11-22 14:28:45 +0200

    Basic bootstrap nav
    for reference

--- header.php
+++ header.php
@@ -27,3 +27,19 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+  <!-- the nav -->
+
+  <?php
+  wp_nav_menu(array(
+    'menu' => 'primary',
+    'theme_location' => 'main',
+    'depth' => 2,
+    'container' => 'div',
+    'container_class' => 'collapse navbar-collapse',
+                    //'container_id'      => 'bs-example-navbar-collapse-1',
+    'menu_class' => 'nav navbar-nav',
+    'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+    'walker' => new wp_bootstrap_navwalker())
+  );
+  ?>
