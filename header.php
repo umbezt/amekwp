226991c7af817b04e0db5adb746ccdc6c95d05f9
Oscar Mwanandimai <oscar.mwana@gmail.com>
2015-07-01 14:32:59 +0200

    Updated to add bower, gulp

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Oscar Mwanandimai"/>
   <title>
     <?php
     global $page, $paged;
@@ -19,10 +20,10 @@
       echo ' | ' . sprintf(__('Page %s', 'amk'), max($paged, $page));
     ?>
   </title>
-  <meta name="author" content=""/>
-  <meta name="copyright" content=""/>
   <link rel="profile" href="http://gmpg.org/xfn/11"/>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+  <link rel="shortcut icon" href="<?php echo IMAGES. 'favicon.ico'; ?>" type="image/x-icon">
+<link rel="icon" href="<?php echo IMAGES. 'favicon.ico'; ?>" type="image/x-icon">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
