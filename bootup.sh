2ff4cc5c8bce55e237f5b25cb25f7c0fa6b63ea3
Oscar Mwanandimai <oscar.mwana@gmail.com>
2015-11-19 12:23:08 +0200

    [sh] remove bootup after initializing project

--- bootup.sh
+++ bootup.sh
@@ -8,3 +8,4 @@ cp -r bower_components/Fedanator/src/* src;
 cp bower_components/Fedanator/gulpfile.js gulpfile.js;
 cp bower_components/Fedanator/package.json package.json;
 npm install;
+rm bootup.sh;
